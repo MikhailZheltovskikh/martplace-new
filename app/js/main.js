acf6ef040b26500d729353b7dc097da36897726b
Mikhail_Zheltovskikh <misha14051994@mail.ru>
2021-08-30 00:54:33 +0500

    added new products

--- app/js/main.js
+++ app/js/main.js
@@ -6,4 +6,13 @@ $ (function(){
       readOnly: true
     });
 
+    $('.featured-product__slider-inner').slick({
+       nextArrow: '<span class="lnr-chevron-left"></span>',
+       prevArrow: '<span class="lnr-chevron-right"></span>',
+    });
+
+
+
+    var mixer = mixitup('.new-products__inner-item');
+
 });
